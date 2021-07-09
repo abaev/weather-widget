b171277fa9c15add0e45174888f6201520b3d0ad
Alexander Abaev <lowrydertrue@gmail.com>
2021-07-09 14:25:09 +1100

    Настроена сборка в bundle

--- craco.config.js
+++ craco.config.js
@@ -6,6 +6,32 @@ module.exports = {
       '@': path.resolve(__dirname, 'src/'),
     // '@Components': path.resolve(__dirname, 'src/components'),
     // '@So_on': path.resolve(__dirname, 'src/so_on'),
-    }
+    },
+
+    configure: {
+      output: {
+        filename: "static/js/[name].js",
+      },
+      optimization: {
+        runtimeChunk: false,
+        splitChunks: {
+          chunks(chunk) {
+            return false;
+          },
+        },
+      },
+    },
   },
+
+  plugins: [
+    {
+      plugin: {
+        overrideWebpackConfig: ({ webpackConfig }) => {
+          webpackConfig.plugins[5].options.filename = "static/css/[name].css";
+          return webpackConfig;
+        },
+      },
+      options: {},
+    },
+  ]
 };
--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@/index.css';
 import Widget from '@/components/Widget/Widget';
-import reportWebVitals from '@/reportWebVitals';
 
+// Это закомментировано для сборки в bundle
+// import reportWebVitals from '@/reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Widget />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = document.body.getElementsByTagName('weather-widget');
+
+[...root].forEach(el => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Widget />
+    </React.StrictMode>,
+    el
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Это закомментировано для сборки в bundle
+// reportWebVitals();
